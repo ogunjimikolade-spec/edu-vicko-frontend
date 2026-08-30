a03ac30c49cae1f71244b0f77b413877f132615e
kolade <ogunjimikolade@gmail.com>
2026-08-30 16:07:15 +0300

    fix: hardcode backend url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-          `${import.meta.env.VITE_API_URL}/api/auth/login`,
+          `https://eduvicko-backend.onrender.com/api/auth/login`,
         {
           email,
           password,
